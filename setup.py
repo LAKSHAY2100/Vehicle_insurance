7f08ee15e07535d4ee1dc46d3bfaeb3aa8542d47
lakshaybajaj210 <lakshay.bajaj@agnext.in>
2026-05-29 13:38:27 +0530

    initial setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    author="Lakshay",
+    author_email="lakshaybajaj210@gmail.com",
+    packages=find_packages()
+)
